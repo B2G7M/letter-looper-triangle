1a8457d690f4256daea6eaeafd398ba0b6f8865e
Amos Marsters <amosmarsters@Amoss-MacBook-Pro.local>
2016-02-26 15:11:39 -0500

    added br

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@ var letter = prompt('type a letter.');
 var n = 0;
 
 for (i = 0; i < 11; i++) {
-  for (n = 0; n < i; n++)
-  document.write(letter + '<br>');
+  for (n = 0; n < i; n++) {
+    document.write(letter);
+  }
+  document.write('<br>');
 }
+
+
+
 
